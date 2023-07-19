bd08ffe88d26b031f95a0391bf3614dc95ba5b38
Camelia03 <camelia_weber@yahoo.com>
2023-07-19 16:26:14 +0000

    deployment commit

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 
 os.environ['DATABASE_URL'] = 'postgres://chtqqncn:H4_TA11pisXgZgT2ArRfrVpTafi2diWt@tyke.db.elephantsql.com/chtqqncn'
 os.environ['SECRET_KEY'] = 'y)fw!=99t#^cc_lv6819s@20u#q#*ptgu!+u8kjg9*nn8f33ij'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://469575875424773:TVcPWjO5rP_ULrqG6RI5TNGiuQE@ddoeilp3b'
