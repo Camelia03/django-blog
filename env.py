4fd9e76458f049fbe39624ede4e105a167a4bd62
Camelia03 <camelia_weber@yahoo.com>
2023-07-19 15:22:46 +0000

    Modifying settings.py

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+
+os.environ['DATABASE_URL'] = 'postgres://chtqqncn:H4_TA11pisXgZgT2ArRfrVpTafi2diWt@tyke.db.elephantsql.com/chtqqncn'
+os.environ['SECRET_KEY'] = 'y)fw!=99t#^cc_lv6819s@20u#q#*ptgu!+u8kjg9*nn8f33ij'
